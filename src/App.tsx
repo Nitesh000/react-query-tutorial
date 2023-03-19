1a6036c534f49a1aa30bcb4fe172c6bfc57fc213
Nitesh000 <ntudu040@gmail.com>
2023-03-19 14:52:57 +0530

    feat(basic usequery): just basics of using of useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
 
 function App() {
-  return <h1>TanStack Query</h1>;
+  const postQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+    // queryFn: () => Promise.reject("Something went wrong"), //while rejection it will show the error message
+  });
+
+  if (postQuery.isLoading) return <h1>Loading...</h1>;
+  if (postQuery.isError) return <pre>{JSON.stringify(postQuery.error)}</pre>;
+
+  return (
+    <div>
+      {postQuery.data.map((post) => {
+        return <div key={post.id}>{post.title}</div>;
+      })}
+    </div>
+  );
+}
+
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export default App;
